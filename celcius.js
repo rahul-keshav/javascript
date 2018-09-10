4a674b00bfccb36fd902fc812628cc66beebcb82
Rahul Kumar Gupta <rr916122@gmail.com>
2018-09-10 06:33:54 +0530

    learned some javascript... :smile:

---
+++ celcius.js
@@ -0,0 +1,4 @@
+let tempInFarenheit=100
+console.log('temp in Farenheit is '+tempInFarenheit)
+var tempInCelcius= (tempInFarenheit-32) / 1.8
+console.log('temp in celcius is ' + tempInCelcius)
---
+++ variable.js
@@ -0,0 +1,7 @@
+let name = 'john'
+let score= 100
+console.log(name)
+let firstName='Jane'
+let lastName='Doe'
+
+console.log(firstName + ' ' + lastName);
